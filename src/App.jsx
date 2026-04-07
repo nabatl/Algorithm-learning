e5c9c887fe29e87b68f744c9e09913ce6716e8b3
Evan <506212883@qq.com>
2026-04-07 22:55:23 +0800

    移除title

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import AlgorithmExamples from './components/AlgorithmExamples'
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1>SHL算法学习平台</h1>
-      </header>
       <main className="app-main">
         <AlgorithmExamples />
       </main>
